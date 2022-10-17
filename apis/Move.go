db2602cca9d4e360da1489bc253837046fe3b479
一只小黑 <yulikai2000@outlook.com>
2022-10-17 17:36:37 +0800

    单个格子移动信息位置更新

---
+++ apis/Move.go
@@ -0,0 +1,36 @@
+package apis
+
+import (
+	"fmt"
+	"github.com/lorenzoyu2000/mmo_game/core"
+	"github.com/lorenzoyu2000/mmo_game/pb"
+	"github.com/lorenzoyu2000/zinx/ziface"
+	"github.com/lorenzoyu2000/zinx/znet"
+	"google.golang.org/protobuf/proto"
+)
+
+/*
+	玩家移动
+*/
+type MoveApi struct {
+	znet.BaseRouter
+}
+
+func (m *MoveApi) Handle(request ziface.IRequest) {
+	pos := &pb.Position{}
+	err := proto.Unmarshal(request.GetData(), pos)
+	if err != nil {
+		fmt.Println("Move unmarshal err: ", err)
+		return
+	}
+
+	// 获取玩家pid
+	pid, err := request.GetConnection().GetProperty("pid")
+	if err != nil {
+		fmt.Println("Move GetProperty err: ", err)
+		return
+	}
+	fmt.Printf("user pid: %d, x: %f, y: %f, z: %f, v:%f\n", pid, pos.X, pos.Y, pos.Z, pos.V)
+	player := core.WorldMgr.Players[pid.(int32)]
+	player.UpdatePos(pos.X, pos.Y, pos.Z, pos.V)
+}
